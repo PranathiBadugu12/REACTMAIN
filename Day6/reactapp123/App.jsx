9db43992a2275c81f745345eccca179bb23f3a0a
PranathiBadugu12 <pranathi12399@gmail.com>
2020-11-04 09:10:38 +0530

    Added Day6,7

---
+++ Day6/reactapp123/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+   render() {
+      
+      return (
+         <div>
+           <table border="3px">
+              <tr>
+                 <td>Hello</td><td>Hello</td>
+              </tr>
+              <tr>
+                 <td>Hello</td><td>Hello</td>
+              </tr>
+           </table>
+         </div>
+      )
+   }
+}
+export default App;
